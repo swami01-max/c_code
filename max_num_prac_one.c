9608db465c9fd27aed9b929bb08b6f92abc1244c
swami01-max <opswamisurve@gmail.com>
2026-01-17 19:45:34 +0530

    Add files via upload

---
+++ max_num_prac_one.c
@@ -0,0 +1,18 @@
+C++ Practical. 1: Find Maximum of Two Numbers Using if...else
+AIM:
+To write a simple C++ program to find the maximum of two numbers using the if...else control
+structure.
+C++ Code:
+#include <iostream.h>
+#include<conio.h>
+Void main()
+{
+int a, b;
+cout << "Enter two numbers: ";
+cin >> a >> b;
+if (a > b)
+cout << "Maximum number is: " << a;
+else
+cout << "Maximum number is: " << b;
+}
+
