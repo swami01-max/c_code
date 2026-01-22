076573836f63c0537b871bb79cff33cee013cb64
swami01-max <opswamisurve@gmail.com>
2026-01-22 23:25:09 +0530

    Add files via upload

---
+++ Sum_of_first_100_nums.c
@@ -0,0 +1,15 @@
+Aim:
+Write a C++ program to find the sum of first 100 numbers using a control structure.
+Code :-
+#include <iostream.h>
+#include <conio.h>
+Void main() {
+Clrscr(); // Clear the screen
+Int I, sum = 0;
+// Using for loop to calculate sum
+For (I = 1; I <= 100; i++) {
+Sum = sum + I;
+}
+Cout << “The sum of first 100 natural numbers is: “ << sum;
+Getch(); // Wait for key press
+}
